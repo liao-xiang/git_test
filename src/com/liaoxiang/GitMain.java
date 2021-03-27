262d20257ed476a05ec5221d20babfc0ecb73371
liao-xiang <61259744+liao-xiang@users.noreply.github.com>
2021-03-27 18:12:45 +0800

    Update GitMain.java

--- src/com/liaoxiang/GitMain.java
+++ src/com/liaoxiang/GitMain.java
@@ -5,5 +5,6 @@ public class GitMain {
         //d9e6ae2f8a73177846ee8e83829456d656a1b8f0
         //https://github.com/liao-xiang/git_test.git
         System.out.println("Git");
+        System.out.println("Hehe");
     }
 }
